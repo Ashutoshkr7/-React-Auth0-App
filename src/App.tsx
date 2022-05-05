099494912ccda3f59035c7ba1c305f5f5876d551
Ashutosh-kr7 <ashutosh.kr@zemosolabs.com>
2022-05-05 18:50:05 +0530

    added logout and user profile component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import "./App.css";
 import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  return (
+  const { user, isAuthenticated } = useAuth0();
+  return isAuthenticated && user !== undefined ? (
     <>
-      <LoginButton />
+      <Profile />
+      <LogoutButton />
     </>
+  ) : (
+    <LoginButton />
   );
 }
 
